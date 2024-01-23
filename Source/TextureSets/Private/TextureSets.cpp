5ba4f945c4b95e302599788978911eec92be11f1
ele <ele@ea.com>
2024-01-23 17:46:16 -0500

    Fixes crash in texture sets post load sequence. The hash required modules that are only loaded in pure editor mode. Standalone game is a variation of editor that wasn't accounted for. Thanks mpalko for the fix.
    [git-p4: change = 356227]

--- Source/TextureSets/Private/TextureSets.cpp
+++ Source/TextureSets/Private/TextureSets.cpp
@@ -10,6 +10,10 @@ void FTextureSetsModule::StartupModule()
 {
 	FString PluginShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("TextureSets"))->GetBaseDir(), TEXT("Shaders"));
 	AddShaderSourceDirectoryMapping(TEXT("/Plugin/TextureSets"), PluginShaderDir);
+
+#if WITH_EDITOR
+	FModuleManager::Get().LoadModule("TextureSetsStandardModules");
+#endif
 }
 
 void FTextureSetsModule::ShutdownModule()
@@ -18,5 +22,5 @@ void FTextureSetsModule::ShutdownModule()
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FTextureSetsModule, TextureSets)
